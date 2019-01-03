f991f11ca817fad1ca635d9ba3dac83d16f08ea4
Jeff Benton <sikletz@gmail.com>
2019-01-03 01:08:45 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
-# def square_array(array)
-#   squares = []
-#   array.each {|x| squares << x*x}
-#   squares
-# end
-
 def square_array(array)
-  array.collect {|x| x*x}
+  squares = []
+  array.each {|x| squares << x*x}
+  squares
 end
+
+# def square_array(array)
+#   array.collect {|x| x*x}
+# end
